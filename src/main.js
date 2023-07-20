501cd9725d04cb2f748c5ab957beed56e08daedf
MaxymZakharkiv <mzaharkiv77@gmail.com>
2023-07-20 21:59:12 +0300

    basic structure components

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+import "./scss/index.scss";
+import { Excel } from "./components/excel/Excel";
+
 const app = document.querySelector("#app");
+
+const excel = new Excel("#app", {
+  components: [],
+});
+
+console.log(excel);
